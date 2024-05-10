f284ce7a9881e98ff45cdc98f2159b09b4f63d7c
random6-xyz <random6.xyz@gmail.com>
2024-05-10 14:30:27 +0900

    chore: check args has query in search books

--- modules/user.py
+++ modules/user.py
@@ -1,8 +1,13 @@
-from flask import render_template
+from flask import render_template, request
 from modules import app
 from databases.db import Database
 
-@app.route("/search")
+
+@app.route("/search", methods=["GET"])
 def search_books():
+    if "query" not in request.args:
+        error_message = '"query" argument missing'
+        return render_template("error.html", data=error_message), 422
+
     Database().execute(";")
     return render_template("search.html")
--- setting/setup.py
+++ setting/setup.py
@@ -5,8 +5,18 @@ from os import getenv
 ip = "0.0.0.0"
 ports = getenv("PORT", 7777)
 
+
 def db_setup():
     db = Database()
 
     # create table if not exist
-    db.execute(";")
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS userbooks \
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, \
+            title TEXT NOT NULL, \
+            writer TEXT NOT NULL,\
+            publisher TEXT NOT NULL,\
+            ISBN TEXT NOT NULL,\
+            pages INTEGERS, number\
+            INTEGERS NOT NULL);"
+    )
