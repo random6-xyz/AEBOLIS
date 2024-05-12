cec26268c7957f5dd66d11eb458cd9cb18b14a39
imStillDebugging <164037070+imStillDebugging@users.noreply.github.com>
2024-05-12 15:38:35 +0900

    restore

--- modules/auth.py
+++ modules/auth.py
@@ -6,28 +6,33 @@ import bcrypt
 import json
 from flask_login import UserMixin
 
+
 # API : return sign up page
 @app.route("/signup")
 def show_sign_up_page():
     return render_template("signup.html")
 
+
 # API : sign up (name, number, password) sha512 + salt
-@app.route("/signup", method = ["post"])
+@app.route("/signup", method=["post"])
 def sign_up():
     username = request.form["username"]
     student_id = int(request.form["student_id"])
     password = request.form["password"]
-    
+
     row = Database().select_account_info(student_id)
-    
+
     if row is not None:
         return jsonify({"message": "이미 사용 중인 학번입니다."}), 409
-    elif not(8 <= password <= 30):
+    elif not (8 <= password <= 30):
         return jsonify({"message": "비밀번호는 8자 이상 30자 이하이어야 합니다."}), 400
     else:
         salt = bcrypt.gensalt()
         hashed_password = hash_password(password, salt)
-        Database().insert_account_info(student_id, username, hashed_password, salt, None)
+        Database().insert_account_info(
+            student_id, username, hashed_password, salt, None
+        )
+
 
 def get_user_info(session):
     if session == "true":
@@ -35,12 +40,15 @@ def get_user_info(session):
     else:
         info = {"role": "user", "code": "22222222"}
     return json.dumps(info)
+
+
 # API : sign in, create session
 # API : sign out, delete session
 # API : delete account
 # API : check session
 
 ################# NON API #######################
+
 
 class User(UserMixin):
     def __init__(self, id, email, name, password):
@@ -55,13 +63,15 @@ class User(UserMixin):
     def __repr__(self):
         return f"USER: {self.id} = {self.name}"
 
+
 # hashing password
-def hash_password(origin_password : str, salt):
+def hash_password(origin_password: str, salt):
     hash_input = (origin_password + salt).encode()
     hash_object = hashlib.sha512()
     hash_object.update(hash_input)
     return hash_object.hexdigest()
 
+
 # check character
-def is_ascii_33_to_126(string : str) -> bool:
-    return all(('!' <= character <= '~') for character in string)
+def is_ascii_33_to_126(string: str) -> bool:
+    return all(("!" <= character <= "~") for character in string)
