02f22b97eb671be6754b2c3dd94c44d50ab6768b
In Jung <wjddls0318@gmail.com>
2024-05-12 13:58:05 +0900

    fixed vulnerable point and appended NOT NULL for essential column

---
+++ setting/setup.py
@@ -0,0 +1,20 @@
+from databases.db import Database
+from os import getenv
+
+# settings
+ip = "0.0.0.0"
+ports = getenv("PORT", 7777)
+
+def db_setup():
+    db = Database()
+
+    # create table if not exist
+    db.execute("""
+                CREATE TABLE IF NOT EXISTS USER(
+                    ID INTEGER PRIMARY KEY,
+                    NAME TEXT NOT NULL, 
+                    HASHED_PASSWORD TEXT NOT NULL,
+                    SALT TEXT NOT NULL,
+                    IS_CONFIRMED INTEGER
+                    );
+                """)
